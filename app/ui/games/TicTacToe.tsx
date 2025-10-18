1e75cdefb118f1e48a10e3920d6c3b269dfaaf7b
calvintaw <190220508+calvintaw@users.noreply.github.com>
2025-10-18 23:55:41 +0630

    finished TicTacToe Bot Section Completely

--- app/ui/games/TicTacToe.tsx
+++ app/ui/games/TicTacToe.tsx
@@ -46,6 +46,8 @@ const TicTacToeHome: React.FC<{ user: User }> = ({ user }) => {
 	const { setPath } = usePathProvider();
 	const [GameStarted, setGameStarted] = useState(false);
 	const [playerTurn, setPlayerTurn] = useState<"X" | "O">("X");
+	const [activeTab, setActiveTab] = useState<string | undefined>("default");
+
 	const [userSymbol, setUserSymbol] = useState<"X" | "O">("X");
 	const [opponent, setOpponent] = useState<string | null>(null);
 	const [winner, setWinner] = useState<"X" | "O" | "Draw" | null>(null);
@@ -139,11 +141,13 @@ const TicTacToeHome: React.FC<{ user: User }> = ({ user }) => {
 	}
 
 	function endGame() {
-		setStatusMessage(null);
+		setPlayerTurn("X");
+		setUserSymbol("X");
+		setOpponent(null);
 		setGameStarted(false);
 		setWinner(null);
-		setOpponent(null);
-		setBoard(() => Array.from({ length: 3 }, () => ["", "", ""]));
+		setBoard(Array.from({ length: 3 }, () => ["", "", ""]));
+		setStatusMessage(null);
 	}
 
 	function sendInvite() {}
@@ -289,148 +293,161 @@ const TicTacToeHome: React.FC<{ user: User }> = ({ user }) => {
 
 			{/* Sidebar Section */}
 			{!GameStarted && (
-				<section className="block bg-contrast border-t md:border-t-0 md:border-l border-border p-4 max-md:pb-20">
-					<h2 className="text-2xl font-bold text-foreground mb-6">Play Tic Tac Toe</h2>
+				<section className="md:w-fit bg-contrast border-t md:border-t-0 md:border-l max-md:pb-10 border-border p-4 h-full flex flex-col justify-between">
+					<div>
+						<h2 className="text-2xl font-bold text-foreground mb-6">Play Tic Tac Toe</h2>
 
-					<Tabs.Root>
-						{/* Hidden triggers just to satisfy Radix */}
-						<Tabs.List>
-							<Tabs.Trigger value="play-bot" asChild>
-								<button className="w-full flex items-center md:max-w-[320px] gap-4 p-4 rounded-xl bg-background hover:bg-secondary transition-all border border-border/50 mb-4 ">
-									<div className="bg-primary/10 p-3 rounded-lg">
-										<FaRobot className="text-primary w-6 h-6" />
-									</div>
-									<div className="flex flex-col text-left">
-										<span className="font-semibold text-foreground">Play Bots</span>
-										<span className="text-muted text-sm">Challenge a bot from Easy to Master</span>
-									</div>
-								</button>
-							</Tabs.Trigger>
-						</Tabs.List>
-						<Dialog.Root>
-							<Dialog.Trigger asChild>
-								<button
-									onClick={async () => {
-										setIsPending(true);
-										const friends = await getOnlineFriends(user.id);
-										setOnlineFriends(friends);
-										setIsPending(false);
-									}}
-									disabled
-									className="w-full flex items-center md:max-w-[320px] gap-4 p-4 rounded-xl bg-background hover:bg-secondary transition-all border border-border/50 disabled:opacity-50 cursor-not-allowed"
-								>
-									<div className="bg-primary/10 p-3 rounded-lg">
-										<FaUserFriends className="text-primary w-6 h-6" />
-									</div>
-									<div className="flex flex-col text-left">
-										<span className="font-semibold text-foreground">Play a Friend</span>
-										<span className="text-muted text-sm text-wrap break-words">
-											Invite a friend to a quick match (Under Construction)
-										</span>
-									</div>
-								</button>
-							</Dialog.Trigger>
+						<Tabs.Root value={activeTab} onValueChange={setActiveTab}>
+							{/* Hidden triggers just to satisfy Radix */}
+							<Tabs.List>
+								<Tabs.Trigger value="play-bot" asChild>
+									<button
+										onClick={() => setActiveTab("play-bot")}
+										className="w-full flex items-center md:max-w-[320px] gap-4 p-4 rounded-xl bg-background hover:bg-secondary transition-all border border-border/50 mb-2 "
+									>
+										<div className="bg-primary/10 p-3 rounded-lg">
+											<FaRobot className="text-primary w-6 h-6" />
+										</div>
+										<div className="flex flex-col text-left">
+											<span className="font-semibold text-foreground">Play Bots</span>
+											<span className="text-muted text-sm">Challenge a bot from Easy to Master</span>
+										</div>
+									</button>
+								</Tabs.Trigger>
+							</Tabs.List>
+							<Dialog.Root>
+								<Dialog.Trigger asChild>
+									<button
+										onClick={async () => {
+											setIsPending(true);
+											const friends = await getOnlineFriends(user.id);
+											setOnlineFriends(friends);
+											setIsPending(false);
+										}}
+										disabled
+										className="w-full flex items-center md:max-w-[320px] gap-4 p-4 rounded-xl bg-background hover:bg-secondary transition-all border border-border/50 disabled:opacity-50 cursor-not-allowed"
+									>
+										<div className="bg-primary/10 p-3 rounded-lg">
+											<FaUserFriends className="text-primary w-6 h-6" />
+										</div>
+										<div className="flex flex-col text-left">
+											<span className="font-semibold text-foreground">Play a Friend</span>
+											<span className="text-muted text-sm text-wrap break-words">
+												Invite a friend to a quick match (Under Construction)
+											</span>
+										</div>
+									</button>
+								</Dialog.Trigger>
 
-							<Dialog.Portal>
-								<Dialog.Overlay className="fixed inset-0 bg-black/50 z-[10000]" />
-								<Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-surface rounded-xl p-6 w-full max-w-md shadow-lg border border-border z-[12000]">
-									<Dialog.Title className="text-base font-semibold text-foreground">Online Friends</Dialog.Title>
-									<Dialog.Description className="sr-only">
-										Invite a friend to play Tic Tac Toe. You can select any online friend and send them a game invite.
-									</Dialog.Description>
+								<Dialog.Portal>
+									<Dialog.Overlay className="fixed inset-0 bg-black/50 z-[10000]" />
+									<Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-surface rounded-xl p-6 w-full max-w-md shadow-lg border border-border z-[12000]">
+										<Dialog.Title className="text-base font-semibold text-foreground">Online Friends</Dialog.Title>
+										<Dialog.Description className="sr-only">
+											Invite a friend to play Tic Tac Toe. You can select any online friend and send them a game invite.
+										</Dialog.Description>
 
-									<hr className="hr-separator my-2 bg-transparent border-contrast border" />
-									{(!isPending ? onlineFriends : Array(2).fill(null)).map((friend, idx) => (
-										<div
-											key={friend?.id ?? idx}
-											className={clsx(
-												"rounded-lg h-15 px-2.5 hover:bg-secondary/75 flex items-center gap-2.5 group/contact",
-												!friend?.id && "bg-secondary/75 mb-1.5"
-											)}
+										<hr className="hr-separator my-2 bg-transparent border-contrast border" />
+										{(!isPending ? onlineFriends : Array(2).fill(null)).map((friend, idx) => (
+											<div
+												key={friend?.id ?? idx}
+												className={clsx(
+													"rounded-lg h-15 px-2.5 hover:bg-secondary/75 flex items-center gap-2.5 group/contact",
+													!friend?.id && "bg-secondary/75 mb-1.5"
+												)}
+											>
+												<div className="h-full mr-1 flex flex-row py-3">
+													{friend ? (
+														<Avatar
+															statusIndicator={true}
+															id={friend.id}
+															src={friend.image}
+															size="size-8"
+															status={true}
+															displayName={friend.displayName}
+														/>
+													) : (
+														<div className="bg-surface animate-pulse w-8 h-8 rounded-full" />
+													)}
+												</div>
+
+												<div className="text-sm h-full flex flex-col justify-center flex-1 font-medium text-text truncate leading-tight">
+													{friend ? (
+														<>
+															<p className="text-base m-0">{friend.displayName}</p>
+															<p className="text-muted text-[13.5px] -mt-0.5">{friend.username}</p>
+														</>
+													) : (
+														<>
+															<div className="h-4 bg-surface animate-pulse rounded w-3/4 mb-1"></div>
+															<div className="h-3 bg-surface animate-pulse rounded w-1/2"></div>
+														</>
+													)}
+												</div>
+
+												{friend && (
+													<button className="align-end btn-secondary-border" onClick={() => sendInvite()}>
+														Invite
+													</button>
+												)}
+											</div>
+										))}
+										{onlineFriends.length <= 0 && !isPending && (
+											<div className="flex flex-col items-center justify-center text-center py-10 px-4">
+												<div className="bg-secondary/40 rounded-full p-4 mb-3">
+													<FaUserFriends className="w-8 h-8 text-muted" />
+												</div>
+												<p className="text-muted text-base font-medium">Looks like no one is online today</p>
+												<p className="text-xs text-muted/70 mt-1">
+													Try again later or invite a friend to join your game!
+												</p>
+											</div>
+										)}
+									</Dialog.Content>
+								</Dialog.Portal>
+							</Dialog.Root>
+
+							{/* Play Bots Content */}
+							<Tabs.Content value="play-bot" asChild>
+								<div className="flex flex-col items-center gap-4 mt-6">
+									<p className="text-lg font-semibold">Choose your symbol</p>
+									<div className="flex gap-4">
+										<button
+											onClick={() => {
+												setPlayerTurn("X");
+												setUserSymbol("X");
+												setOpponent("Bot");
+												setGameStarted(true);
+											}}
+											className="btn-primary px-4 py-2"
 										>
-											<div className="h-full mr-1 flex flex-row py-3">
-												{friend ? (
-													<Avatar
-														statusIndicator={true}
-														id={friend.id}
-														src={friend.image}
-														size="size-8"
-														status={true}
-														displayName={friend.displayName}
-													/>
-												) : (
-													<div className="bg-surface animate-pulse w-8 h-8 rounded-full" />
-												)}
-											</div>
-
-											<div className="text-sm h-full flex flex-col justify-center flex-1 font-medium text-text truncate leading-tight">
-												{friend ? (
-													<>
-														<p className="text-base m-0">{friend.displayName}</p>
-														<p className="text-muted text-[13.5px] -mt-0.5">{friend.username}</p>
-													</>
-												) : (
-													<>
-														<div className="h-4 bg-surface animate-pulse rounded w-3/4 mb-1"></div>
-														<div className="h-3 bg-surface animate-pulse rounded w-1/2"></div>
-													</>
-												)}
-											</div>
-
-											{friend && (
-												<button className="align-end btn-secondary-border" onClick={() => sendInvite()}>
-													Invite
-												</button>
-											)}
-										</div>
-									))}
-									{onlineFriends.length <= 0 && !isPending && (
-										<div className="flex flex-col items-center justify-center text-center py-10 px-4">
-											<div className="bg-secondary/40 rounded-full p-4 mb-3">
-												<FaUserFriends className="w-8 h-8 text-muted" />
-											</div>
-											<p className="text-muted text-base font-medium">Looks like no one is online today</p>
-											<p className="text-xs text-muted/70 mt-1">
-												Try again later or invite a friend to join your game!
-											</p>
-										</div>
-									)}
-								</Dialog.Content>
-							</Dialog.Portal>
-						</Dialog.Root>
-
-						{/* Play Bots Content */}
-						<Tabs.Content value="play-bot" asChild>
-							<div className="flex flex-col items-center gap-4 mt-6">
-								<p className="text-lg font-semibold">Choose your symbol</p>
-								<div className="flex gap-4">
-									<button
-										onClick={() => {
-											setPlayerTurn("X");
-											setUserSymbol("X");
-											setOpponent("Bot");
-											setGameStarted(true);
-										}}
-										className="btn-primary px-4 py-2"
-									>
-										X (go first)
-									</button>
-									<button
-										onClick={() => {
-											setPlayerTurn("O");
-											setUserSymbol("O");
-											setOpponent("Bot");
-											setGameStarted(true);
-											setTimeout(() => botFirstMove("X"), 200);
-										}}
-										className="btn-primary px-4 py-2"
-									>
-										O (go second)
-									</button>
+											X (go first)
+										</button>
+										<button
+											onClick={() => {
+												setPlayerTurn("O");
+												setUserSymbol("O");
+												setOpponent("Bot");
+												setGameStarted(true);
+												setTimeout(() => botFirstMove("X"), 200);
+											}}
+											className="btn-primary px-4 py-2"
+										>
+											O (go second)
+										</button>
+									</div>
 								</div>
-							</div>
-						</Tabs.Content>
-					</Tabs.Root>
+							</Tabs.Content>
+
+							<Tabs.Content value="default"></Tabs.Content>
+						</Tabs.Root>
+					</div>
+
+					{activeTab !== "de" && (
+						<button onClick={() => setActiveTab("default")} className="btn-secondary-border mt-auto max-md:mt-15">
+							Cancel
+						</button>
+					)}
 				</section>
 			)}
 		</div>
